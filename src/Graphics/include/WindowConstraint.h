6c6d2848268c157db4ada4210e123e0ac873b74b
madguy187 <nubbiehood@hotmail.com>
2021-05-19 21:47:41 +0800

    testtest

--- src/Graphics/include/WindowConstraint.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "imgui.h"
-
-namespace CustomConstraint
-{
-  void ResizeCallBack(ImGuiSizeCallbackData* data);
-}
-
--- src/Graphics/source/stb_image.cpp
+++
@@ -1,4 +0,0 @@
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "pch.h"
-#include "stb_image.h"
