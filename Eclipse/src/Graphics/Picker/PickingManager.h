c5c710199fad30201358e96e7be1a4e45ab383d9
xShiori <rachel.lin5494@gmail.com>
2021-07-10 21:46:49 +0800

    Intermittent commit

---
+++ Eclipse/src/Graphics/Picker/PickingManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef PICKINGMANAGER_H
+#define PICKINGMANAGER_H
+
+namespace Eclipse
+{
+	class PickingManager
+	{
+		void GenerateAAbb();
+	};
+}
+
+#endif  /* PICKINGMANAGER_H */
