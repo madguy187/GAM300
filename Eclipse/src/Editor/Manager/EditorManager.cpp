5f40a321c3f238817c53a8924167de6c1fe52609
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2021-07-25 19:15:54 +0800

    Update EditorManager.cpp

--- Eclipse/src/Editor/Manager/EditorManager.cpp
+++ Eclipse/src/Editor/Manager/EditorManager.cpp
@@ -70,7 +70,8 @@ namespace Eclipse
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-		glTexImage2D(GL_TEXTURE_2D, 0, bytes_per_pixels, width, height, 0, (bytes_per_pixels == 3) ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+		glTexImage2D(GL_TEXTURE_2D, 0, bytes_per_pixels, width, height, 0, 
+			(bytes_per_pixels == 3) ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
 		io.Fonts->SetTexID((void*)static_cast<size_t>(textureID));
 
