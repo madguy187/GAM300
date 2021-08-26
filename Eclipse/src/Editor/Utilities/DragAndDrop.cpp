ec611bc08c4371e17c645ceb273ce36c0658e8e2
byungb <mtianyu1@gmail.com>
2021-08-26 12:54:43 +0800

    folder drag and drop almost done

--- Eclipse/src/Editor/Utilities/DragAndDrop.cpp
+++ Eclipse/src/Editor/Utilities/DragAndDrop.cpp
@@ -70,55 +70,132 @@ namespace Eclipse
 					{
 						std::string parentPath = std::filesystem::path(itemPaths / paths).string();
 						static std::string folderName;
+						bool baseFile = false;
 						size_t pos = 0;
+						int fileSize = 0;
 						parentPath = parentPath.substr(0, parentPath.find_last_of("/\\"));
-						std::string EndingPath = dirEntry.path().filename().string();
-						EndingPath = EndingPath.substr(0, EndingPath.find_last_of("/\\"));
-						std::string token = EndingPath;
+						if(parentPath ==  AssetPath)
+						{
+							parentPath = std::filesystem::path(itemPaths / paths).string();
+							baseFile = true;
+						}
 						for (auto const& pair : pathMap)
 						{
 							if (pair.first == std::filesystem::path(parentPath.c_str()))
 							{
+								for (const auto& file : std::filesystem::recursive_directory_iterator(std::filesystem::path(itemPaths / paths)))
+								{
+									fileSize++;
+								}
 								for (auto const& pair2 : pair.second)
 								{
-									size_t found = pair2.string().find(paths);
-									if (found != std::string::npos)
+									if (std::filesystem::is_directory(pair2))
 									{
-										deletefiles.insert(std::pair<std::string, std::string>(pair2.string(), pair2.string()));
-										folderName = pair2.string();
-										pos = folderName.find(parentPath);
-										if (pos != std::string::npos)
+										size_t found = pair2.string().find(paths);
+										if (found != std::string::npos)
 										{
-											// If found then erase it from string
-											folderName.erase(pos, parentPath.length());
-										}
-
-										if (std::filesystem::is_directory(pair2))
-										{
+											fileSize--;
+											deletefiles.insert(std::pair<std::string, std::string>(pair2.string(), pair2.string()));
+											folderName = pair2.string();
+											pos = folderName.find(parentPath);
+											if (pos != std::string::npos)
+											{
+												// If found then erase it from string
+												folderName.erase(pos, parentPath.length());
+											}
 											if (files.find((parentPath + folderName).c_str()) != files.end())
 											{
 												folderName = files.at((parentPath + folderName).c_str());
 											}
 											else
 											{
-												files.insert(std::pair<std::string, std::string>(dirEntry.path().string() + folderName, dirEntry.path().string() + "\\" + folderName));
+												if(baseFile)
+												{
+													files.insert(std::pair<std::string, std::string>(dirEntry.path().string()+ "\\"  + std::filesystem::path(parentPath).filename().string() + folderName, dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string() + folderName));
+												}
+												else
+												{
+													files.insert(std::pair<std::string, std::string>(dirEntry.path().string() + folderName, dirEntry.path().string() + folderName));
+												}
 											}
-											std::filesystem::create_directories(dirEntry.path().string() + folderName);
-										}
-										else
-										{
-											std::filesystem::copy(pair2, dirEntry.path().string() + folderName);
+											if (baseFile)
+											{
+												std::filesystem::create_directories(dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string());
+												std::filesystem::create_directories(dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string() + folderName);
+											}
+											else
+											{
+												std::filesystem::create_directories(dirEntry.path().string() + folderName);
+											}
 										}
 									}
-								}
-							}
+									else
+									{
+										size_t found = pair2.string().find(paths);
+										if (found != std::string::npos)
+										{
+											fileSize--;
+											deletefiles.insert(std::pair<std::string, std::string>(pair2.string(), pair2.string()));
+											folderName = pair2.string();
+											pos = folderName.find(parentPath);
+											if (pos != std::string::npos)
+											{
+												// If found then erase it from string
+												folderName.erase(pos, parentPath.length());
+											}
+											
+											if (baseFile)
+											{
+												deletefiles.insert(std::pair<std::string, std::string>(pair2.string(), pair2.string()));
+												if (files.find((parentPath + folderName).c_str()) != files.end())
+												{
+													folderName = files.at((parentPath + folderName).c_str());
+												}
+												else
+												{
+													files.insert(std::pair<std::string, std::string>(dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string() + folderName, dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string() + folderName));
+												}
+												std::filesystem::create_directories(dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string());
+												std::filesystem::create_directories(dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string() + folderName);
+												std::filesystem::copy(pair2, dirEntry.path().string() + "\\" + std::filesystem::path(parentPath).filename().string() + folderName);
 
-							for (auto const& it : deletefiles)
-							{
-								std::filesystem::remove_all(std::filesystem::path(it.first.c_str()));
+											}
+											else
+											{
+												
+												if (files.find((parentPath + folderName).c_str()) != files.end())
+												{
+													folderName = files.at((parentPath + folderName).c_str());
+												}
+												else
+												{
+													files.insert(std::pair<std::string, std::string>(dirEntry.path().string() + folderName, dirEntry.path().string() + folderName));
+												}
+												std::filesystem::copy(pair2, dirEntry.path().string() + folderName);
+											}
+										}
+									}
+									if(fileSize == -1 )
+									{
+										break;
+									}
+								}
+								for (auto const& it : deletefiles)
+								{
+									std::filesystem::remove_all(std::filesystem::path(it.first.c_str()));
+								}
+								if (baseFile)
+								{
+									baseFile = false;
+									std::filesystem::remove(std::filesystem::path(parentPath));
+								}
+								//resetting for the next moving of files
+								deletefiles.clear();
+								files.clear();
+								folderName.clear();
+								parentPath.clear();
+								refreshBrowser = true;
 							}
-							deletefiles.clear();
-							files.clear();
 						}
 					}
 					else
