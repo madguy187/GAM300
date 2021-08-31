c798d650fda069fef1bc036f55f7d7716541e2ae
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2021-09-01 05:36:53 +0800

    Update CommandHistory.cpp

--- Eclipse/src/Editor/Utilities/UndoAndRedo/CommandHistory.cpp
+++ Eclipse/src/Editor/Utilities/UndoAndRedo/CommandHistory.cpp
@@ -10,6 +10,7 @@ namespace Eclipse
 	{
 		cmd->execute();
 
+		// Delete all the existing commands infront when a new command is done
 		if (m_CommandPtrIndex < m_CommandSize - 1)
 		{
 			for (int i = m_CommandSize - 1; i > m_CommandPtrIndex; i--)
@@ -18,24 +19,30 @@ namespace Eclipse
 			m_CommandSize = m_CommandPtrIndex + 1;
 		}
 
+		// Insert command into array
 		m_Commands[m_CommandSize] = cmd;
 		m_CommandSize++;
 
+		// Check if the command before and the current one are able to merge
 		if (m_CommandSize > 1 && m_Commands[m_CommandSize - 1]->CanMerge() && 
 			m_Commands[m_CommandSize - 2]->CanMerge())
 		{
+			// Check if merge is successful
 			if (m_Commands[m_CommandSize - 1]->MergeCmds(m_Commands[m_CommandSize - 2]))
 			{
+				// Delete the current one if successful after merging
 				delete m_Commands[m_CommandSize - 1];
 				m_CommandSize--;
 			}
 		}
 
+		// Decrement Pointer Index
 		m_CommandPtrIndex = m_CommandSize - 1;
 	}
 
 	void CommandHistory::DisableMergeForMostRecendCommand()
 	{
+		// Manually Stop merging all commands
 		if ((m_CommandSize - 1) >= 0)
 			m_Commands[m_CommandSize - 1]->DisableMerge();
 	}
