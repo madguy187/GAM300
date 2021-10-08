230da7581fcaef2ad83090c6812c1844c0927703
ghthegreat <56424881+ghthegreat@users.noreply.github.com>
2021-10-08 22:31:54 +0800

    added to scale according to transform

--- Eclipse/src/ECS/SystemManager/Systems/System/Collision/CollisionSystem.cpp
+++ Eclipse/src/ECS/SystemManager/Systems/System/Collision/CollisionSystem.cpp
@@ -9,14 +9,20 @@ namespace Eclipse
 		engine->Timer.tracker.system_start = static_cast<float>(glfwGetTime());
 		for (auto const& entity : mEntities)
 		{
-			engine->gPhysics.InitActor(entity);
-			engine->gPhysics.CreateShape(entity);
-			engine->gPhysics.UpdateShapes(entity);
-			engine->gPhysics.ChangeType(entity);
-			if (!engine->world.CheckComponent<RigidBodyComponent>(entity))
-			{
-				engine->gPhysics.UpdateActor(entity);
+			engine->gPhysics.UpdateVariables(entity);
+
+			if(engine->IsScenePlaying())
+			{ 
+				engine->gPhysics.InitActor(entity);
+				engine->gPhysics.CreateShape(entity);
+				engine->gPhysics.UpdateShapes(entity);
+				engine->gPhysics.ChangeType(entity);
+				if (!engine->world.CheckComponent<RigidBodyComponent>(entity))
+				{
+					engine->gPhysics.UpdateActor(entity);
+				}
 			}
+			
 		}
 		engine->Timer.tracker.system_end = static_cast<float>(glfwGetTime());
 		engine->Timer.UpdateTimeContainer(engine->Timer.tracker);
