dd090f6eea5c897665331c3079438868c4e345c2
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2021-10-24 15:57:06 +0800

    Update ControllerDriver.cpp

--- Eclipse/src/OpenVR/Drivers/Controller/ControllerDriver.cpp
+++ Eclipse/src/OpenVR/Drivers/Controller/ControllerDriver.cpp
@@ -11,8 +11,15 @@ namespace Eclipse
 		// Retrieves container object that stores information about my driver
 		PropertyContainerHandle_t props = VRProperties()->TrackedDeviceToPropertyContainer(DriverID);
 
-		// Tell OpenVR where to get my driver's Input Profile
+		// Telling OpenVR where to get my driver's Input Profile
 		VRProperties()->SetStringProperty(props, Prop_InputProfilePath_String, "{EclipseEngine}/input/controller_profile.json");
+		// Telling OpenVR what kind of device this is
+		// Will be changing it constantly for testing purposes
+		// End game, for our gam, it should be TrackedControllerRole_OptOut, I think LOL
+		VRProperties()->SetInt32Property(props, Prop_ControllerRoleHint_Int32, ETrackedControllerRole::TrackedControllerRole_OptOut);
+		// Sets up handler, using to send joystick commands to OpenVR with, in the Y direction (forward/backward)
+		VRDriverInput()->CreateScalarComponent(props, "/input/joystick/y", &JoystickYHandle, EVRScalarType::VRScalarType_Absolute,
+			EVRScalarUnits::VRScalarUnits_NormalizedTwoSided);
 
 		return VRInitError_None;
 	}
