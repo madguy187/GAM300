47af672f437375b675b98ff69c93afb27d86b577
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2021-10-24 13:01:00 +0800

    Material editor placeholder done

--- Eclipse/src/Editor/Windows/MaterialEditor/MaterialEditor.cpp
+++ Eclipse/src/Editor/Windows/MaterialEditor/MaterialEditor.cpp
@@ -8,12 +8,16 @@ namespace Eclipse
 		if (IsVisible)
 		{
 			ECGui::DrawMainWindow<void()>(WindowName, std::bind(&MaterialEditorWindow::RunMainWindow, this));
-			ECGui::DrawMainWindow<void()>("Mesh Settings", std::bind(&MaterialEditorWindow::RunMaterialSettings, this));
+			ECGui::DrawMainWindow<void()>("Material Settings", std::bind(&MaterialEditorWindow::RunMaterialSettings, this));
 		}
 	}
 
 	void MaterialEditorWindow::Init()
 	{
+		Type = EditorWindowType::EWT_MATERIALEDITOR;
+		WindowName = "Material Editor";
+		m_frameBuffer = engine->gFrameBufferManager->GetFramebuffer(FrameBufferMode::FBM_MATERIALEDITOR);
+		IsVisible = true;
 	}
 
 	void MaterialEditorWindow::Unload()
@@ -22,10 +26,27 @@ namespace Eclipse
 
 	void MaterialEditorWindow::RunMainWindow()
 	{
+		ImVec2 viewportPanelSize = ECGui::GetWindowSize();
+
+		if (mViewportSize.getX() != viewportPanelSize.x ||
+			mViewportSize.getY() != viewportPanelSize.y)
+		{
+			m_frameBuffer->Resize(static_cast<unsigned>(viewportPanelSize.x),
+				static_cast<unsigned>(viewportPanelSize.y), FrameBufferMode::FBM_MATERIALEDITOR);
+			mViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
+			engine->gFrameBufferManager->UpdateAspectRatio(FrameBufferMode::FBM_MATERIALEDITOR, mViewportSize);
+		}
+
+		ChildSettings settings;
+		settings.Name = "MaterialEditorFrameBuffer";
+		settings.Size = ImVec2{ mViewportSize.x, mViewportSize.y };
+		ECGui::DrawChildWindow<void()>(settings, std::bind(&MaterialEditorWindow::RunFrameBuffer, this));
 	}
 
 	void MaterialEditorWindow::RunFrameBuffer()
 	{
+		ECGui::Image((void*)(static_cast<size_t>(m_frameBuffer->GetTextureColourBufferID())),
+			ImVec2{ mViewportSize.x, mViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 	}
 
 	void MaterialEditorWindow::RunMaterialSettings()
