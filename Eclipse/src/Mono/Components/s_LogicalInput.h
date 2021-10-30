272f5f8d679785611240658558551a999a58b585
Darren Sim <darrensim59@gmail.com>
2021-10-31 05:11:36 +0800

    For Nico

--- Eclipse/src/Mono/Components/s_LogicalInput.h
+++ Eclipse/src/Mono/Components/s_LogicalInput.h
@@ -1,9 +1,48 @@
 #pragma once
 #include "pch.h"
 #include "mono/jit/jit.h"
-#include "Global.h"
 
 namespace Eclipse
 {
+	static void GetKeyTriggered(InputKeycode KeyIn)
+	{
+		engine->InputManager->GetKeyTriggered(KeyIn);
+	}
+
+	static void GetKeyTriggered(std::string Mappedkeycode)
+	{
+		engine->InputManager->GetKeyTriggered(Mappedkeycode);
+	}
+
+	static void GetKeyCurrent(InputKeycode keycode)
+	{
+		engine->InputManager->GetKeyCurrent(keycode);
+	}
+
+	static void GetKeyCurrent(std::string Mappedkeycode)
+	{
+		engine->InputManager->GetKeyCurrent(Mappedkeycode);
+	}
+
+	// I haven do UI FOR MOUSE YET
+	static void GetMouseTriggered(InputMouseKeycode KeyIn)
+	{
+		engine->InputManager->GetMouseTriggered(KeyIn);
+	}
+
+	static void GetMouseTriggered(std::string Mappedkeycode)
+	{
+		engine->InputManager->GetMouseTriggered(Mappedkeycode);
+	}
+
+	static void GetMouseCurrent(InputMouseKeycode keycode)
+	{
+		engine->InputManager->GetMouseCurrent(keycode);
+	}
+
+	static void GetMouseCurrent(std::string Mappedkeycode)
+	{
+		engine->InputManager->GetMouseCurrent(Mappedkeycode);
+	}
 
 }
