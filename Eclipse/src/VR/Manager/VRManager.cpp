a65791977a4755845b75d8043dcf53d5f67486dd
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2021-11-02 16:31:52 +0800

    mgmghj

--- Eclipse/src/VR/Manager/VRManager.cpp
+++ Eclipse/src/VR/Manager/VRManager.cpp
@@ -4,151 +4,151 @@
 
 namespace Eclipse
 {
-	bool VRManager::Init()
-	{
-		// Loading the SteamVR Runtime
-		EVRInitError eError = VRInitError_None;
-		m_pHMD = VR_Init(&eError, VRApplication_Scene);
+	//bool VRManager::Init()
+	//{
+	//	// Loading the SteamVR Runtime
+	//	EVRInitError eError = VRInitError_None;
+	//	m_pHMD = VR_Init(&eError, VRApplication_Scene);
 
-		if (eError != VRInitError_None)
-		{
-			m_pHMD = NULL;
-			char buffer[1024];
-			sprintf_s(buffer, sizeof(buffer), "Unable to init VR runtime: %s",
-				VR_GetVRInitErrorAsEnglishDescription(eError));
+	//	if (eError != VRInitError_None)
+	//	{
+	//		m_pHMD = NULL;
+	//		char buffer[1024];
+	//		sprintf_s(buffer, sizeof(buffer), "Unable to init VR runtime: %s",
+	//			VR_GetVRInitErrorAsEnglishDescription(eError));
 
-			return false;
-		}
+	//		return false;
+	//	}
 
-		m_strDriver = "No Driver";
-		m_strDisplay = "No Display";
+	//	m_strDriver = "No Driver";
+	//	m_strDisplay = "No Display";
 
-		m_strDriver = GetTrackedDeviceString(k_unTrackedDeviceIndex_Hmd, Prop_TrackingSystemName_String);
-		m_strDisplay = GetTrackedDeviceString(k_unTrackedDeviceIndex_Hmd, Prop_SerialNumber_String);
+	//	m_strDriver = GetTrackedDeviceString(k_unTrackedDeviceIndex_Hmd, Prop_TrackingSystemName_String);
+	//	m_strDisplay = GetTrackedDeviceString(k_unTrackedDeviceIndex_Hmd, Prop_SerialNumber_String);
 
-		// Compositior makes sense of all of the VR Overlays and put them together to create a realistic VR visual
-		// Failure of initialization means it can't perform its role of displaying intended visual
-		if (!InitCompositor())
-		{
-			std::cout << "Failed to initialize VR Compositor!" << std::endl;
-			return false;
-		}
+	//	// Compositior makes sense of all of the VR Overlays and put them together to create a realistic VR visual
+	//	// Failure of initialization means it can't perform its role of displaying intended visual
+	//	if (!InitCompositor())
+	//	{
+	//		std::cout << "Failed to initialize VR Compositor!" << std::endl;
+	//		return false;
+	//	}
 
-		VRInput()->SetActionManifestPath(Path_MakeAbsolute("../hellovr_actions.json", Path_StripFilename(Path_GetExecutablePath())).c_str());
-		vr::VRInput()->GetActionHandle("/actions/demo/in/AnalogInput", &m_actionAnalogInput);
+	//	VRInput()->SetActionManifestPath(Path_MakeAbsolute("../hellovr_actions.json", Path_StripFilename(Path_GetExecutablePath())).c_str());
+	//	vr::VRInput()->GetActionHandle("/actions/demo/in/AnalogInput", &m_actionAnalogInput);
 
-		vr::VRInput()->GetActionSetHandle("/actions/demo", &m_actionsetDemo);
+	//	vr::VRInput()->GetActionSetHandle("/actions/demo", &m_actionsetDemo);
 
-		vr::VRInput()->GetInputSourceHandle("/user/hand/left", &m_rHand[Left].m_source);
-		vr::VRInput()->GetActionHandle("/actions/demo/in/Hand_Left", &m_rHand[Left].m_actionPose);
+	//	vr::VRInput()->GetInputSourceHandle("/user/hand/left", &m_rHand[Left].m_source);
+	//	vr::VRInput()->GetActionHandle("/actions/demo/in/Hand_Left", &m_rHand[Left].m_actionPose);
 
-		vr::VRInput()->GetInputSourceHandle("/user/hand/right", &m_rHand[Right].m_source);
-		vr::VRInput()->GetActionHandle("/actions/demo/in/Hand_Right", &m_rHand[Right].m_actionPose);
+	//	vr::VRInput()->GetInputSourceHandle("/user/hand/right", &m_rHand[Right].m_source);
+	//	vr::VRInput()->GetActionHandle("/actions/demo/in/Hand_Right", &m_rHand[Right].m_actionPose);
 
-		return true;
-	}
+	//	return true;
+	//}
 
-	bool VRManager::InitCompositor()
-	{
-		EVRInitError peError = VRInitError_None;
+	//bool VRManager::InitCompositor()
+	//{
+	//	EVRInitError peError = VRInitError_None;
 
-		if (!VRCompositor())
-		{
-			printf("Compositor intialization failed. See log file for details\n");
-			return false;
-		}
+	//	if (!VRCompositor())
+	//	{
+	//		printf("Compositor intialization failed. See log file for details\n");
+	//		return false;
+	//	}
 
-		return true;
-	}
+	//	return true;
+	//}
 
-	void VRManager::Shutdown()
-	{
-		if (m_pHMD)
-		{
-			VR_Shutdown();
-			m_pHMD = NULL;
-		}
-	}
+	//void VRManager::Shutdown()
+	//{
+	//	if (m_pHMD)
+	//	{
+	//		VR_Shutdown();
+	//		m_pHMD = NULL;
+	//	}
+	//}
 
-	void VRManager::ProcessVREvent(const VREvent_t& event)
-	{
-		switch (event.eventType)
-		{
-		case VREvent_TrackedDeviceDeactivated:
-			printf("Device %u detached.\n", event.trackedDeviceIndex);
-			break;
-		case VREvent_TrackedDeviceUpdated:
-			printf("Device %u updated.\n", event.trackedDeviceIndex);
-			break;
-		default:
-			break;
-		}
-	}
+	//void VRManager::ProcessVREvent(const VREvent_t& event)
+	//{
+	//	switch (event.eventType)
+	//	{
+	//	case VREvent_TrackedDeviceDeactivated:
+	//		printf("Device %u detached.\n", event.trackedDeviceIndex);
+	//		break;
+	//	case VREvent_TrackedDeviceUpdated:
+	//		printf("Device %u updated.\n", event.trackedDeviceIndex);
+	//		break;
+	//	default:
+	//		break;
+	//	}
+	//}
 
-	void VRManager::RenderFrame()
-	{
-	}
+	//void VRManager::RenderFrame()
+	//{
+	//}
 
-	void VRManager::RenderControllerAxes()
-	{
-	}
+	//void VRManager::RenderControllerAxes()
+	//{
+	//}
 
-	bool VRManager::SetupStereoRenderTargets()
-	{
-		return false;
-	}
+	//bool VRManager::SetupStereoRenderTargets()
+	//{
+	//	return false;
+	//}
 
-	void VRManager::SetupCameras()
-	{
-	}
+	//void VRManager::SetupCameras()
+	//{
+	//}
 
-	void VRManager::RenderStereoTargets()
-	{
-	}
+	//void VRManager::RenderStereoTargets()
+	//{
+	//}
 
-	void VRManager::RenderScene(Hmd_Eye neye)
-	{
-	}
+	//void VRManager::RenderScene(Hmd_Eye neye)
+	//{
+	//}
 
-	std::string VRManager::GetTrackedDeviceString(TrackedDeviceIndex_t unDevice, 
-		TrackedDeviceProperty prop, TrackedPropertyError* peError)
-	{
-		uint32_t unRequiredBufferLen = VRSystem()->GetStringTrackedDeviceProperty(unDevice,
-			prop, NULL, 0, peError);
+	//std::string VRManager::GetTrackedDeviceString(TrackedDeviceIndex_t unDevice, 
+	//	TrackedDeviceProperty prop, TrackedPropertyError* peError)
+	//{
+	//	uint32_t unRequiredBufferLen = VRSystem()->GetStringTrackedDeviceProperty(unDevice,
+	//		prop, NULL, 0, peError);
 
-		if (!unRequiredBufferLen)
-			return "";
+	//	if (!unRequiredBufferLen)
+	//		return "";
 
-		char* pchBuffer = new char[unRequiredBufferLen];
-		unRequiredBufferLen = VRSystem()->GetStringTrackedDeviceProperty(unDevice, prop,
-			pchBuffer, unRequiredBufferLen, peError);
-		std::string sResult = pchBuffer;
-		delete []pchBuffer;
+	//	char* pchBuffer = new char[unRequiredBufferLen];
+	//	unRequiredBufferLen = VRSystem()->GetStringTrackedDeviceProperty(unDevice, prop,
+	//		pchBuffer, unRequiredBufferLen, peError);
+	//	std::string sResult = pchBuffer;
+	//	delete []pchBuffer;
 
-		return sResult;
-	}
+	//	return sResult;
+	//}
 
-	glm::mat4 VRManager::GetHMDMatrixProjectionEye(Hmd_Eye nEye)
-	{
-		return glm::mat4();
-	}
+	//glm::mat4 VRManager::GetHMDMatrixProjectionEye(Hmd_Eye nEye)
+	//{
+	//	return glm::mat4();
+	//}
 
-	glm::mat4 VRManager::GetHMDMatrixPoseEye(Hmd_Eye nEye)
-	{
-		return glm::mat4();
-	}
+	//glm::mat4 VRManager::GetHMDMatrixPoseEye(Hmd_Eye nEye)
+	//{
+	//	return glm::mat4();
+	//}
 
-	glm::mat4 VRManager::GetCurrentViewProjectionMatrix(Hmd_Eye nEye)
-	{
-		return glm::mat4();
-	}
+	//glm::mat4 VRManager::GetCurrentViewProjectionMatrix(Hmd_Eye nEye)
+	//{
+	//	return glm::mat4();
+	//}
 
-	void VRManager::UpdateHMDMatrixPose()
-	{
-	}
-	glm::mat4 VRManager::ConvertSteamVRMatrixToMatrix4(const HmdMatrix34_t& matPose)
-	{
-		return glm::mat4();
-	}
+	//void VRManager::UpdateHMDMatrixPose()
+	//{
+	//}
+	//glm::mat4 VRManager::ConvertSteamVRMatrixToMatrix4(const HmdMatrix34_t& matPose)
+	//{
+	//	return glm::mat4();
+	//}
 }
 
--- Eclipse/src/VR/Manager/VRManager.h
+++ Eclipse/src/VR/Manager/VRManager.h
@@ -6,98 +6,193 @@ namespace Eclipse
 {
 	using namespace vr;
 
+	//class VRManager
+	//{
+	//public:
+	//	bool Init();
+	//	bool InitCompositor();
+
+	//	void Shutdown();
+
+	//	void ProcessVREvent(const VREvent_t& event);
+	//	void RenderFrame();
+
+	//	void RenderControllerAxes();
+
+	//	bool SetupStereoRenderTargets();
+	//	void SetupCameras();
+
+	//	void RenderStereoTargets();
+	//	void RenderScene(Hmd_Eye neye);
+
+	//	glm::mat4 GetHMDMatrixProjectionEye(Hmd_Eye nEye);
+	//	glm::mat4 GetHMDMatrixPoseEye(Hmd_Eye nEye);
+	//	glm::mat4 GetCurrentViewProjectionMatrix(Hmd_Eye nEye);
+	//	void UpdateHMDMatrixPose();
+
+	//	glm::mat4 ConvertSteamVRMatrixToMatrix4(const HmdMatrix34_t& matPose);
+	//private:
+	//	/**
+	//	* Essential VR Instances
+	//	**/
+	//	IVRSystem* m_pHMD;
+	//	std::string m_strDriver;
+	//	std::string m_strDisplay;
+	//	TrackedDevicePose_t m_rTrackedDevicePose[k_unMaxTrackedDeviceCount];
+	//	glm::mat4 m_rmat4DevicePose[k_unMaxTrackedDeviceCount];
+
+	//	struct ControllerInfo_t
+	//	{
+	//		VRInputValueHandle_t m_source{ k_ulInvalidInputValueHandle };
+	//		VRActionHandle_t m_actionPose{ k_ulInvalidActionHandle };
+	//		VRActionHandle_t m_actionHaptic{ k_ulInvalidActionHandle };
+	//		glm::mat4 m_rmat4Pose;
+	//		bool m_bShowController;
+	//	};
+
+	//	enum EHand
+	//	{
+	//		Left = 0,
+	//		Right = 1,
+	//	};
+	//	ControllerInfo_t m_rHand[2];
+
+	//	/**
+	//	* OpenGL BookKeeping 
+	//	**/
+	//	int m_iTrackedControllerCount;
+	//	int m_iTrackedControllerCount_Last;
+	//	int m_iValidPoseCount;
+	//	int m_iValidPoseCount_Last;
+	//	bool m_bShowCubes;
+	//	glm::vec2 m_vAnalogValue;
+
+	//	// What classes we saw poses for this frame
+	//	std::string m_strPoseClasses;
+	//	// For each device, a character representing its class
+	//	char m_rDevClassChar[k_unMaxTrackedDeviceCount];
+
+	//	float m_fNearClip;
+	//	float m_fFarClip;
+
+	//	GLuint m_glControllerVertBuffer;
+	//	GLuint m_unControllerVAO;
+	//	unsigned int m_uiControllerVercount;
+
+	//	glm::mat4 m_mat4HMDPose;
+	//	glm::mat4 m_mat4eyePosLeft;
+	//	glm::mat4 m_mat4eyePosRight;
+
+	//	glm::mat4 m_mat4ProjectionCenter;
+	//	glm::mat4 m_mat4ProjectionLeft;
+	//	glm::mat4 m_mat4ProjectionRight;
+
+	//	GLuint m_unControllerTransformProgramID;
+	//	GLuint m_nControllerMatrixLocation;
+
+	//	VRActionHandle_t m_actionHideCubes{ k_ulInvalidActionHandle };
+	//	VRActionHandle_t m_actionHideThisController{ k_ulInvalidActionHandle };
+	//	VRActionHandle_t m_actionTriggerHaptic{ k_ulInvalidActionHandle };
+	//	VRActionHandle_t m_actionAnalogInput{ k_ulInvalidActionHandle };
+
+	//	VRActionHandle_t m_actionsetDemo{ k_ulInvalidActionHandle };
+
+	//	std::string GetTrackedDeviceString(TrackedDeviceIndex_t unDevice,
+	//		TrackedDeviceProperty prop, TrackedPropertyError* peError = NULL);
+	//};
+
 	class VRManager
 	{
 	public:
+		VRManager();
 		bool Init();
-		bool InitCompositor();
+		bool Update();
+		bool Destroy();
 
-		void Shutdown();
+		/* To retrieve the model of the tracked device from SteamVR */
+		// bVRRenderModel* FindOrLoadRenderModel(const char* renderModelString);
 
-		void ProcessVREvent(const VREvent_t& event);
-		void RenderFrame();
+		std::string GetTrackedDeviceString(IVRSystem* pHmd, TrackedDeviceIndex_t unDevice,
+			TrackedDeviceProperty prop, TrackedPropertyError* peError = NULL);
 
-		void RenderControllerAxes();
+		~VRManager();
 
-		bool SetupStereoRenderTargets();
+		struct FrameBufferDesc
+		{
+			GLuint m_nDepthBufferID{ 0 };
+			GLuint m_nRenderTextureID{ 0 };
+			GLuint m_nRenderFrameBufferID{ 0 };
+			GLuint m_nResolveTextureID{ 0 };
+			GLuint m_nResolveFrameBufferID{ 0 };
+		};
+
+	protected:
+		/* Initialize VR Components */
+		bool InitializeComponents();
+		bool InitVRCompositor();
+		bool InitRenderModels();
+		bool InitEyeBuffer(int nWidth, int nHeight, FrameBufferDesc& fbDesc);
+
 		void SetupCameras();
+		bool SetupStereoRenderTargets();
+		void SetupCompanionWindow();
+		void SetupRenderModels();
+		void SetupRenderModelForTrackedDevice(TrackedDeviceIndex_t unTrackedDeviceIndex);
 
+		/* Render VR Components*/
+		void RenderControllerAxes();
+		void RenderToTV();
 		void RenderStereoTargets();
-		void RenderScene(Hmd_Eye neye);
+		void RenderCompanionWindow();
+
+		void RenderScene(vr::Hmd_Eye nEye);
+
+		glm::mat4 GetCurrentViewProjectionMatrix(vr::Hmd_Eye nEye);
+		glm::mat4 GetCurrentViewMatrix(vr::Hmd_Eye nEye);
+		glm::mat4 GetCurrentProjectionMatrix(vr::Hmd_Eye nEye);
+
+		void UpdateHMDMatrixPose();
+
+		/* VR Utilities */
+		glm::mat4 Mat4FromSteamVRMatrix(const HmdMatrix34_t& matPose);
+
+		glm::vec3 GetControllerPosition(TrackedDeviceIndex_t trackedDeviceIndex);
+
+		glm::vec3 GetControllerRaycastDirection(TrackedDeviceIndex_t trackedDeviceIndex);
 
 		glm::mat4 GetHMDMatrixProjectionEye(Hmd_Eye nEye);
 		glm::mat4 GetHMDMatrixPoseEye(Hmd_Eye nEye);
-		glm::mat4 GetCurrentViewProjectionMatrix(Hmd_Eye nEye);
-		void UpdateHMDMatrixPose();
 
-		glm::mat4 ConvertSteamVRMatrixToMatrix4(const HmdMatrix34_t& matPose);
+		/* Event Processing */
+		void PollVREvent();
+		void ProcessButtonEvent(VREvent_t event);
+
 	private:
-		/**
-		* Essential VR Instances
-		**/
-		IVRSystem* m_pHMD;
-		std::string m_strDriver;
-		std::string m_strDisplay;
-		TrackedDevicePose_t m_rTrackedDevicePose[k_unMaxTrackedDeviceCount];
-		glm::mat4 m_rmat4DevicePose[k_unMaxTrackedDeviceCount];
+		FrameBufferDesc leftEyeDesc;
+		FrameBufferDesc rightEyeDesc;
 
-		struct ControllerInfo_t
-		{
-			VRInputValueHandle_t m_source{ k_ulInvalidInputValueHandle };
-			VRActionHandle_t m_actionPose{ k_ulInvalidActionHandle };
-			VRActionHandle_t m_actionHaptic{ k_ulInvalidActionHandle };
-			glm::mat4 m_rmat4Pose;
-			bool m_bShowController;
-		};
+		/* VR System */
+		IVRSystem* pHmd = NULL; // pointer to Head Mounted Display
+		EVRInitError eError = VRInitError_None;
+		IVRRenderModels* m_pRenderModels = NULL;
+		bool m_rbShowTrackedDevice[vr::k_unMaxTrackedDeviceCount];
 
-		enum EHand
-		{
-			Left = 0,
-			Right = 1,
-		};
-		ControllerInfo_t m_rHand[2];
+		/* VR Picking */
+		// ObjectPickingVR* Picker = nullptr;
 
-		/**
-		* OpenGL BookKeeping 
-		**/
-		int m_iTrackedControllerCount;
-		int m_iTrackedControllerCount_Last;
-		int m_iValidPoseCount;
-		int m_iValidPoseCount_Last;
-		bool m_bShowCubes;
-		glm::vec2 m_vAnalogValue;
+		/* VR Shaders */
+		// Shader* VRcontrollerShader = nullptr;
+		// Shader* VRrendermodelShader = nullptr;
+		// Shader* VRcompanionwindowShader = nullptr;
+		
+		// Shader* tvShader = nullptr;
 
-		// What classes we saw poses for this frame
-		std::string m_strPoseClasses;
-		// For each device, a character representing its class
-		char m_rDevClassChar[k_unMaxTrackedDeviceCount];
+		/* VR Cameras */
+		// Camera* leftEyeCamera = nullptr;
+		// Camera* rightEyeCamera = nullptr;
 
-		float m_fNearClip;
-		float m_fFarClip;
-
-		GLuint m_glControllerVertBuffer;
-		GLuint m_unControllerVAO;
-		unsigned int m_uiControllerVercount;
-
-		glm::mat4 m_mat4HMDPose;
-		glm::mat4 m_mat4eyePosLeft;
-		glm::mat4 m_mat4eyePosRight;
-
-		glm::mat4 m_mat4ProjectionCenter;
-		glm::mat4 m_mat4ProjectionLeft;
-		glm::mat4 m_mat4ProjectionRight;
-
-		GLuint m_unControllerTransformProgramID;
-		GLuint m_nControllerMatrixLocation;
-
-		VRActionHandle_t m_actionHideCubes{ k_ulInvalidActionHandle };
-		VRActionHandle_t m_actionHideThisController{ k_ulInvalidActionHandle };
-		VRActionHandle_t m_actionTriggerHaptic{ k_ulInvalidActionHandle };
-		VRActionHandle_t m_actionAnalogInput{ k_ulInvalidActionHandle };
-
-		VRActionHandle_t m_actionsetDemo{ k_ulInvalidActionHandle };
-
-		std::string GetTrackedDeviceString(TrackedDeviceIndex_t unDevice,
-			TrackedDeviceProperty prop, TrackedPropertyError* peError = NULL);
+		/* VR FBOS for Eyes */
+		// PostProcessing* leftEyeFBO = nullptr;
+		// PostProcessing* rightEyeFBO = nullptr;
 	};
 }
