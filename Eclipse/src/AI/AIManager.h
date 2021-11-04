47d236cfc5ce5111d3adc216f40e537c571cabbd
ghthegreat <56424881+ghthegreat@users.noreply.github.com>
2021-11-04 18:44:06 +0800

    bump

--- Eclipse/src/AI/AIManager.h
+++ Eclipse/src/AI/AIManager.h
@@ -1,52 +1,13 @@
 #pragma once
 #include "ECS/ComponentManager/Components/AIComponent.h"
-using ASNodeID = uint16_t;
-namespace Eclipse
+class AIManager
 {
-	struct grid
-	{
-		float _squareDist{ 0 };
-	};
-
-	struct BaseNode
-	{
-		ASNodeID _ID;
-		ECVec3 Position;
-		int hcost;
-		int gcost;
-		int fcost;
-		BaseNode* prev;
-	};
-
-	struct NeighbourInfo
-	{
-		BaseNode* neighbour;
-		int distfromNeighbour;
-	};
-
-	struct AstarNode
-	{
-		BaseNode* _Node;
-		std::vector<NeighbourInfo> neighbours;
-		
-	};
-
-	struct AstarPathComponent
-	{
-
-
-	};
-
-
-	class AIManager
-	{
-	private:
-		AstarNode current;
-		std::list<AstarNode*> openlist;
-		std::list<AstarNode*> closedlist;
-		std::vector<AstarNode> _Nodes;
-	public:
-		/*AstarNode* GetNextCurrent();
-		void CalculatePath(AstarNode& start, AstarNode& end);*/
-	};
-}
+public:
+	void patrol(Entity ent);
+	void AddWaypoint(Entity AItoaddto, Entity waypointent);
+	void AddTargetPointEntity(Entity ent);
+	void RemoveDeletedWaypoints(Entity ent, Entity deleteent);
+	const std::vector<Entity>& GetTargetPoints();
+private:
+	std::vector<Entity> TargetPoints;
+};
