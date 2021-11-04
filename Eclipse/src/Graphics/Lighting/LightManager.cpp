96178a86153f7e7eb0a1b673191e9774818f9f32
Darren Sim <darrensim59@gmail.com>
2021-11-05 04:39:17 +0800

    up

--- Eclipse/src/Graphics/Lighting/LightManager.cpp
+++ Eclipse/src/Graphics/Lighting/LightManager.cpp
@@ -114,6 +114,7 @@ namespace Eclipse
     void LightManager::SpotLightUpdate(Entity ID)
     {
         float UPWARD = 0.0f;
+        float RightVec = 0.0f;
         glm::vec2 NewDir = { 0,0 };
         int Negative = 0;
 
@@ -138,18 +139,21 @@ namespace Eclipse
             UPWARD = tanf(glm::radians(abs(transCom.rotation.getX()))) * -1 * Negative;
             NewDir = glm::vec2(1, 0) + +glm::vec2(0, UPWARD);
         }
+
         //quad 3
         if (transCom.rotation.getX() > 181 && transCom.rotation.getX() < 270 || transCom.rotation.getX() < -180 && transCom.rotation.getX() > -270)
         {
             UPWARD = tanf(glm::radians(abs(transCom.rotation.getX()))) * -1 * Negative;
             NewDir = glm::vec2(1, 0) + glm::vec2(0, UPWARD);
         }
+
         //quad4
         if (transCom.rotation.getX() > 270 && transCom.rotation.getX() < 360 || transCom.rotation.getX() < -270 && transCom.rotation.getX() > -360)
         {
             UPWARD = tanf(glm::radians(abs(transCom.rotation.getX()))) * Negative;
             NewDir = glm::vec2(-1, 0) + glm::vec2(0, UPWARD);
         }
+
         //quad1
         if (transCom.rotation.getX() > 0 && transCom.rotation.getX() < 90 || transCom.rotation.getX() < 0 && transCom.rotation.getX() > -90)
         {
@@ -158,35 +162,41 @@ namespace Eclipse
         }
 
         glm::vec2 ScaleupDir = NewDir * GetMagnitudeOfDirection;
+        SpotLight_.direction.setX(0.0f);
         SpotLight_.direction.setY(ScaleupDir.y);
         SpotLight_.direction.setZ(ScaleupDir.x);
 
         if (transCom.rotation.getX() == 270.0f || transCom.rotation.getX() == -270.0f)
         {
+            SpotLight_.direction.setX(0.0f);
             SpotLight_.direction.setY(-5.0f * Negative);
             SpotLight_.direction.setZ(0.0f);
         }
 
         if (transCom.rotation.getX() == 0.0f)
         {
+            SpotLight_.direction.setX(0.0f);
             SpotLight_.direction.setY(0.0f);
             SpotLight_.direction.setZ(-5.0f);
         }
 
         if (transCom.rotation.getX() == 90.0f || transCom.rotation.getX() == -90.0f)
         {
+            SpotLight_.direction.setX(0.0f);
             SpotLight_.direction.setY(5.0f * Negative);
             SpotLight_.direction.setZ(0.0f);
         }
 
         if (transCom.rotation.getX() == 180.0f || transCom.rotation.getX() == -180.0f)
         {
+            SpotLight_.direction.setX(0.0f);
             SpotLight_.direction.setY(0.0f);
             SpotLight_.direction.setZ(5.0f);
         }
 
         if (transCom.rotation.getX() == 360.0f || transCom.rotation.getX() == -360.0f)
         {
+            SpotLight_.direction.setX(0.0f);
             SpotLight_.direction.setY(0.0f);
             SpotLight_.direction.setZ(-5.0f);
         }
