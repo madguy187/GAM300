bd3f66213113037a8bf083de30a99417245b7559
byungb <mtianyu1@gmail.com>
2021-11-06 22:10:17 +0800

    fixed the memleak

--- Eclipse/src/Editor/Windows/MaterialEditor/MaterialEditor.h
+++ Eclipse/src/Editor/Windows/MaterialEditor/MaterialEditor.h
@@ -6,10 +6,10 @@ namespace Eclipse
 {
 	class MaterialEditorWindow final : public ECGuiWindow
 	{
-		MaterialNode MaterialEditor;
 		std::string tempName;
 		bool nameChanged = false;
 	public:
+		MaterialNode MaterialEditor;
 		void Update() override;
 		void Init() override;
 		void Unload() override;
@@ -19,13 +19,12 @@ namespace Eclipse
 		void OnCameraMoveEvent();
 		void RunMaterialSettings();
 		void CheckCurrentMaterial(size_t comboIndex);
-
+		~MaterialEditorWindow();
 		ECVec3 ColorPicker{ 1.0f };
 	private:
 		FrameBuffer* m_frameBuffer{ nullptr };
 		ECVec2 mViewportSize{ 0.f, 0.f };
 		size_t comboindex = 0;
-
 		void Buttons();
 		void InitMaterialNodes();
 		bool ShowTransformProperty(const char* name, ImGuiTextFilter& filter);
