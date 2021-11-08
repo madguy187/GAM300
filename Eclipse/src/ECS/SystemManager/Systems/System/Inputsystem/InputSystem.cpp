10e971a77b27568d0f77110eb6b1f5ac04d9eeae
Darren Sim <darrensim59@gmail.com>
2021-11-08 23:40:55 +0800

    Update

--- Eclipse/src/ECS/SystemManager/Systems/System/Inputsystem/InputSystem.cpp
+++ Eclipse/src/ECS/SystemManager/Systems/System/Inputsystem/InputSystem.cpp
@@ -5,16 +5,6 @@ namespace Eclipse
 {
     void InputSystem::Update()
     {
-        if (engine->InputManager->GetKeyTriggered("Test"))
-        {
-            engine->InputManager->LockCursor(CursorLockMode::Locked);
-        }
-
-        if (engine->InputManager->GetKeyTriggered(InputKeycode::Key_RIGHT))
-        {
-            engine->InputManager->LockCursor(CursorLockMode::None);
-        }
-
         engine->InputManager->CursorUpdate();
         engine->InputManager->AxisUpdate();
         engine->InputManager->UpdateKeys();
