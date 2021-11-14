6f4d7d265306b81932278dce1355e8f554138e2d
madguy187 <nubbiehood@hotmail.com>
2021-11-14 15:42:06 +0800

    able to update vars when script updates with NO CRASH

--- Eclipse/src/Assets/Scripts/Test3.cs
+++ Eclipse/src/Assets/Scripts/Test3.cs
@@ -7,7 +7,13 @@ public class Test3 : EclipseBehavior
 {
     bool iscreated = false;
 
+    public float Test;
     public GameObject Spot;
     public GameObject STUPIDDARREN;
     public Light light;
+
+    void Update()
+    {
+        Console.WriteLine(Test);
+    }
 }
