093b26e888f9790c041d2aaa06c6dbac2454333b
Darren Sim <darrensim59@gmail.com>
2021-11-14 20:04:26 +0800

    update

--- Eclipse/src/ECS/ComponentManager/Components/SpotLightComponent.h
+++ Eclipse/src/ECS/ComponentManager/Components/SpotLightComponent.h
@@ -20,11 +20,11 @@ namespace Eclipse
         float IntensityStrength = 0.0f;
         float radius = 0.0f;
         float cutOff = 7.5f;
-        float outerCutOff = 13.5f;
+        float outerCutOff = 10.5f;
         float constant = 1.0f;
         float linear = 0.09f;
         float quadratic = 0.032f;
-        int AttenuationLevel = 2;
+        int AttenuationLevel = 1;
         float SurroundingAttenuationLevel = 0.3f;
 
         bool EnableBlinnPhong = true;
