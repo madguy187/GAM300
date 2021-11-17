8c5aed842b052154a49bd17797e370081bb5eeb8
Darren Sim <darrensim59@gmail.com>
2021-11-17 19:15:47 +0800

    Update

--- Eclipse/src/ECS/SystemManager/Systems/System/MaterialSystem/MaterialSystem.cpp
+++ Eclipse/src/ECS/SystemManager/Systems/System/MaterialSystem/MaterialSystem.cpp
@@ -19,24 +19,6 @@ namespace Eclipse
             engine->Timer.SetName({ SystemName::MATERIAL });
         engine->Timer.tracker.system_start = static_cast<float>(glfwGetTime());
 
-        auto* MaterialEditor = engine->editorManager->GetEditorWindow<MaterialEditorWindow>();
-
-        if (engine->InputManager->GetKeyTriggered(InputKeycode::Key_M))
-        {
-            if (!MaterialEditor->IsVisible)
-            {
-                MaterialEditor->IsVisible = true;
-            }
-            else
-            {
-                // Whenever we open up the material Editor , we Will Clear it.
-                //MaterialEditor->ColorPicker = ECVec3{ 1.0f };
-                //engine->gPBRManager->gMaterialEditorSettings->ClearCurrentMaterial();
-                //engine->gPBRManager->gMaterialEditorSettings->ClearTextureFields();
-                MaterialEditor->IsVisible = false;
-            }
-        }
-
         // Material Node Editor
         engine->gPBRManager->gMaterialEditorSettings->RenderMaterialScene();    
         engine->MaterialManager.StencilBufferClear();
